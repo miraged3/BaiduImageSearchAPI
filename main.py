43ed5acc72bb75a17f25bcf27b45181a07586355
Maooookai <1220568032@qq.com>
2021-10-21 00:12:52 +0800

    bug fix

--- main.py
+++ main.py
@@ -18,29 +18,36 @@ def baidu():
     options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
-    options.add_argument('blink-settings=imagesEnabled=false')
     options.add_argument('--disable-gpu')
+    options.add_argument("--window-size=1920,1050")
     browser = webdriver.Chrome(executable_path=driverLocation, chrome_options=options)
     browser.get('https://image.baidu.com')
     browser.find_element(by=By.XPATH, value='//span/input[@class="s_ipt"]').send_keys(data.get('keyword'))
     browser.find_element(by=By.XPATH, value='//span/input[@class="s_newBtn"]').click()
     image = browser.find_element(by=By.XPATH, value='//li[@class="imgitem"][' + str(
-        random.randint(2, 6)) + ']/div/div/a/img').get_attribute('src')
+        random.randint(2, 5)) + ']/div/div/a/img').get_attribute('src')
     if str(image).startswith('http'):
         address = urllib.request.urlopen(str(image))
         img = address.read()
         encoded_image = str(base64.b64encode(img))
         result = {
-            'image': 'data:image/jpg;base64,' + encoded_image.split("'")[1]
+            'image': encoded_image.split("'")[1]
         }
         return json.dumps(result, ensure_ascii=False)
     else:
-        result = {
-            'image': str(image)
-        }
-        browser.quit()
-        return json.dumps(result, ensure_ascii=False)
+        if 'data:image/jpg;base64,' in str(image):
+            result = {
+                'image': str(image).replace('data:image/jpg;base64,', '')
+            }
+            browser.quit()
+            return json.dumps(result, ensure_ascii=False)
+        else:
+            result = {
+                'image': str(image).replace('data:image/jpeg;base64,', '')
+            }
+            browser.quit()
+            return json.dumps(result, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=318, debug=False)
+    app.run(host='0.0.0.0', port=318)
